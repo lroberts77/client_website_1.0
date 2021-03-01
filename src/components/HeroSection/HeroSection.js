ebe581ef3baeb3f3e734640977ce3e1fd432e491
Liam Roberts <liamroberts77@googlemail.com>
2021-03-01 20:00:48 +0000

    added folders for pages and herosection and created herosection.js and .css files

---
+++ src/components/HeroSection/HeroSection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default HeroSection
