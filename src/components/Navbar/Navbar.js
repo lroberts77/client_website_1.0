404dc26f35b0f49cb29b4a238dbc9730595afccc
Liam Roberts <liamroberts77@googlemail.com>
2021-03-03 15:46:47 +0000

    added dependancies for fontawesome

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,18 +5,24 @@ import nature from "../../assets/nature.jpg"
 function Navbar() {
     return (
         <nav className="navbar">
-            {/* <div className="logo"> */}
+            <div className="logo">
                 <img className="img" src={nature} alt="logo" />
-            {/* </div> */}
+            </div>
             <ul className="nav-ul">
                 <li><a href="">HOME</a></li>
                 <li><a href="">ABOUT</a></li>
                 <li><a href="">SERVICES</a></li>
                 <li><a href="">CONTACT</a></li>
             </ul>
+            <ul className="ul-links">
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+                <li><a href=""></a></li>
+            </ul>
         </nav>
     )
 }
+{/* <i class="fab fa-instagram"></i> */}
 
 export default Navbar
 
