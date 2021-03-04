0278f9e675df0a9d79af145554cceb74408f2dae
Liam Roberts <liamroberts77@googlemail.com>
2021-03-04 20:02:23 +0000

    added styling change for navbar on screen scroll

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,31 +1,57 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Navbar.css";
 import nature from "../../assets/nature.jpg"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faYoutube, faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
-  
+
+function Navbar() {
+  // const [navbar, setNavbar] = useState(false)
+
+  // const changeBG = () => {
+  //   console.log(window.scrollY);
+  // }
+
+  // window.addEventListener('scroll', changeBG);
+
+    const [navbar, setnavbar] = useState("navbar");
+    
+    const listenScrollEvent = () => {
+      console.log(window.scrollY);
+      if (window.scrollY < 73) {
+        return setnavbar("navbar")
+      } else if (window.scrollY > 70) {
+        return setnavbar("navbar-black")
+      } 
+    }
+    
+    useEffect(() => {
+      window.addEventListener('scroll', listenScrollEvent);
+    
+      return () =>
+        window.removeEventListener('scroll', listenScrollEvent);
+    }, []);
 
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <div className="logo">
-                <img className="img" src={nature} alt="logo" />
-                <h1 className="owner">by Catalina Balan</h1>
-            </div>
-            <ul className="nav-ul">
-                <li><a href="">HOME</a></li>
-                <li><a href="">ABOUT</a></li>
-                <li><a href="">SERVICES</a></li>
-                <li><a href="">CONTACT</a></li>
-            </ul>
-            <ul className="ul-links">
-                <li><a href="" ><FontAwesomeIcon icon={faInstagram} className="instagram" size="2x" /></a></li>
-                <li><a href="" className="youtube"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></li>
-                <li><a href="" className="facebook"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></li>
-            </ul>
-        </nav>
-    )
+
+  return (
+      <nav className={navbar}>
+          <div className="logo">
+              <img className="img" src={nature} alt="logo" />
+              <h1 className="owner">by Catalina Balan</h1>
+          </div>
+          <ul className="nav-ul">
+              <li><a href="">HOME</a></li>
+              <li><a href="">ABOUT</a></li>
+              <li><a href="">SERVICES</a></li>
+              <li><a href="">CONTACT</a></li>
+          </ul>
+          <ul className="ul-links">
+              <li><a href="" ><FontAwesomeIcon icon={faInstagram} className="instagram" size="2x" /></a></li>
+              <li><a href="" className="youtube"><FontAwesomeIcon icon={faYoutube} size="2x" /></a></li>
+              <li><a href="" className="facebook"><FontAwesomeIcon icon={faFacebook} size="2x" /></a></li>
+          </ul>
+      </nav>
+  )
 }
 
 export default Navbar
