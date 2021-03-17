60951e29a2e308384164322b7e792bf96114fe00
Liam Roberts <liamroberts77@googlemail.com>
2021-03-17 17:04:22 +0000

    removed some of the text from sit-about and removed margins

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -20,7 +20,7 @@ function HeroSection() {
                         Experiment Growth
                     </h1>
                     <p className="site-about">
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
                     </p>
                 </div>
             </div>
