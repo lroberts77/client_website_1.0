1fc2c7294e094c2bb2e1272af27bfc8958b0854d
Liam Roberts <liamroberts77@googlemail.com>
2021-04-01 21:22:03 +0100

    using react router for page changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
-import HeroSection from './components/HeroSection/HeroSection';
 import Navbar from './components/Navbar/Navbar';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './components/Pages/Home/Home';
+import Products from './components/Pages/About/About';
+import SignUp from './components/Pages/Contact/Contact';
+import Services from './components/Pages/Services/Services';
 
 function App() {
   return (
     <>
-    <Navbar />
-    <HeroSection />
+    {/* <Navbar />
+    <HeroSection /> */}
 
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
+      </Switch>
+    </Router>
     </>
   );
 }
